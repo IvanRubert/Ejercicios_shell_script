35f7a38d5085ed7a69ec9cd8b866ad4f9ad72537
IvanRubert <114068487+IvanRubert@users.noreply.github.com>
2023-02-19 17:10:15 +0100

    Update ejercicio1.sh

--- UT8P1/ejercicio1.sh
+++ UT8P1/ejercicio1.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #Introduce 2 parametros y te dice cual es mayor
-if [[ $1 -lt $2 ]]; then
-  echo "$1 es menor que $2"
-elif [[ $1 -gt $2 ]]; then
-  echo "$1 es mayor que $2"
+if [[ $1 =~ ^-?[0-9]+$ ]] then
+  if [[ $2 =~ ^-?[0-9]+$ ]] then
+    if [[ $1 -lt $2 ]]; then
+      echo "$1 es menor que $2"
+    elif [[ $1 -gt $2 ]]; then
+      echo "$1 es mayor que $2"
+    else
+      echo "$1 es igual a $2"
+    fi
+  else
+    echo "El segundo parametro no es un numero entero"
+  fi
 else
-  echo "$1 es igual a $2"
+  echo "El primer parametro no es un numero entero"
 fi
