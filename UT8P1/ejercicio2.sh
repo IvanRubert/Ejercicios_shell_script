b78ac9efef7eecffc84258fa789bb0a56639af85
IvanRubert <114068487+IvanRubert@users.noreply.github.com>
2023-02-19 17:37:04 +0100

    Añadido los archivos UT8P1

--- UT8P1/ejercicio2.sh
+++ UT8P1/ejercicio2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #Sumar 2 numeros pasados por parametro
 if [[ $1 && $2 ]]; then
-  ((sum=$1+$2))
-  echo "$1 + $2 = $sum"
+  if [[ $1 =~ ^-?[0-9]+.?[0-9]+$ ]]; then
+    if [[ $2 =~ ^-?[0-9]+.?[0-9]+$ ]]; then
+      sum=$(echo "$1 + $2" | bc)
+        echo "$1 + $2 = $sum"
+    else
+      echo "El segundo parametro debe ser un numero"
+    fi
 else
   echo "Error"
   echo "num1 y num2 sustituir por numeros"
