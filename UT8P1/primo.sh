643974639258b5b49e07a438314d62717ad6410a
IvanRubert <114068487+IvanRubert@users.noreply.github.com>
2023-02-21 09:04:16 +0100

    Añadido los archivos UT8P1

--- UT8P1/primo.sh
+++ UT8P1/primo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Verificar numero pasado por parametros si es primo o no
+if [[ $1 =~ ^-?[0-9]+$ ]]; then
 for((i=2; i<=$1/2; i++))
 do
-  ans=$(( $1%i ))
+  ((ans=$1%i))
   if [ $ans -eq 0 ]
   then
     echo "$1 no es un numero primo"
@@ -10,3 +11,6 @@ do
   fi
 done
 echo "$1 es un numero primo"
+else
+echo "El primer parametro debe ser un numero"
+fi
